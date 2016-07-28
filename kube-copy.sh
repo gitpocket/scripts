80e486da285e75e6ae86c38ec31049abf42e327a
Dennis Non <dennis.non@kpn.com>
2016-07-28 08:39:53 +0200

    add kube copy script

---
+++ kube-copy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  echo "Enter the address of your CoreOS master:"
+  read ADDRESS
+else
+  ADDRESS=$1
+fi
+
+echo "Copying KubeCTL"
+scp core@$ADDRESS:/opt/bin/kubectl /usr/bin/kubectl
+chmod 0755 /usr/bin/kubectl
+
