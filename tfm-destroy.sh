9e213d5bea1e170f6892b255b538294be5c813d9
Dennis Non <dennis.non@kpn.com>
2016-08-08 12:40:39 +0200

    update scripts

--- tfm-destroy.sh
+++ tfm-destroy.sh
@@ -7,7 +7,7 @@ else
   PREFIX=${1}
 fi
 
-$STATEFILE="/home/${USER}/github/appfactory/appfactory-poc/terraform/${PREFIX}.tfstate"
+STATEFILE="/home/${USER}/github/appfactory/appfactory-poc/terraform/${PREFIX}.tfstate"
 
 terraform destroy -var customer-prefix=${PREFIX} -var etcd-cluster-token=0 -var tradi-count=0 -state=${STATEFILE}
 
