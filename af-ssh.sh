23fe211bcc675bfe9594cc269cfee61c25779749
Dennis Non <dennis.non@kpn.com>
2016-08-19 10:45:06 +0200

    remove obsolete scripts

--- af-ssh.sh
+++ af-ssh.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-IPADDRESS=$1
+if [ -z $1 ]
+then
+  echo "$(tput setaf 2)Enter IP address:$(tput sgr0)"
+  read IPADDRESS
+else
+  IPADDRESS=$1
+fi
 
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o CheckHostIP=no core@${IPADDRESS}
--- tf-show-ip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-terraform show ~/github/appfactory/appfactory-poc/terraform/terraform.tfstate | grep "^[[:space:]]*ip_address"
