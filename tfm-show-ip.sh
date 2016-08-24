c10f8749a58bebe49fae96d07ddf14ab5c477e27
Dennis Non <dennis.non@kpn.com>
2016-08-24 11:35:41 +0200

    update scripts

--- tfm-show-ip.sh
+++ tfm-show-ip.sh
@@ -7,10 +7,10 @@ then
 else
   CUSTOMERPREFIX=$1
 fi
+TFDIR="/home/${USER}/github/appfactory/appfactory-poc/terraform/"
 
-# terraform show ~/github/appfactory/appfactory-poc/terraform/$PREFIX.tfstate | grep "^[[:space:]]*ip_address"
-terraform get ../terraform/ &>/dev/null
+terraform get ${TFDIR} &>/dev/null
 echo "$(tput setaf 2)Master(s):$(tput sgr0)"
-terraform output -state=/home/$USER/github/appfactory/appfactory-poc/terraform/${CUSTOMERPREFIX}.tfstate -module=kpnappfactory k8s-master.public_ips | sed 's/,/\n/g'
+terraform output -state=${TFDIR}${CUSTOMERPREFIX}.tfstate -module=kpnappfactory k8s-master.public_ips | sed 's/,/\n/g'
 echo "$(tput setaf 2)Nodes:$(tput sgr0)"
-terraform output -state=/home/$USER/github/appfactory/appfactory-poc/terraform/${CUSTOMERPREFIX}.tfstate -module=kpnappfactory k8s-node.public_ips | sed 's/,/\n/g'
+terraform output -state=${TFDIR}${CUSTOMERPREFIX}.tfstate -module=kpnappfactory k8s-node.public_ips | sed 's/,/\n/g'
