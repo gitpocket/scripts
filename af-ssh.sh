ff1a619ea7445e776560c117fa37b03eaa715913
Dennis Non <dennis.non@kpn.com>
2016-09-14 11:57:23 +0200

    update scripts

--- af-ssh.sh
+++ af-ssh.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 if [ -z $1 ]
 then
-  echo "$(tput setaf 2)Enter IP address:$(tput sgr0)"
-  read IPADDRESS
+  echo "$(tput setaf 2)Enter custmer prefix:$(tput sgr0)"
+  read CUSTOMERPREFIX
 else
-  IPADDRESS=$1
+  CUSTOMERPREFIX=$1
 fi
 
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o CheckHostIP=no core@${IPADDRESS}
+TFDIR="/home/${USER}/src/github.com/nautsio/appfactory-poc/terraform/"
+MODULE=$(cat ${TFDIR}/${CUSTOMERPREFIX}.tfstate | jq '.modules[1]["path"][1]' | sed 's/"//g')
+
+terraform get ${TFDIR} &>/dev/null
+MASTER=$(terraform output -state=${TFDIR}${CUSTOMERPREFIX}.tfstate -module=${MODULE} k8s-master.public_ips | awk -F"," '{print $1}')
+
+ssh -A -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o CheckHostIP=no -L 8080:127.0.0.1:8080 core@${MASTER}
--- tfm-show-ip.sh
+++ tfm-show-ip.sh
@@ -9,17 +9,18 @@ else
   CUSTOMERPREFIX=${1}
 fi
 
-if grep -A2 "\"path\"\: \[" ~/appfactory/terraform/${CUSTOMERPREFIX}.tfstate | grep vpc &>/dev/null
-then
-  MODULE="vpc"
-else
-  MODULE="kpnappfactory"
-fi
+#if grep -A2 "^[[:space:]]*\"path\"\:[[:space:]]\[" ~/appfactory/terraform/${CUSTOMERPREFIX}.tfstate | grep vpc &>/dev/null
+#then
+#  MODULE="vpc"
+#else
+#  MODULE="kpnappfactory"
+#fi
 
-TFDIR="/home/${USER}/src/github.com/nautsio/appfactory-poc/terraform/"
+TFDIR="/home/${USER}/src/github.com/nautsio/appfactory-poc/terraform"
+MODULE=$(cat ${TFDIR}/${CUSTOMERPREFIX}.tfstate | jq '.modules[1]["path"][1]' | sed 's/"//g')
 
 terraform get ${TFDIR} &>/dev/null
 echo "$(tput setaf 2)Master(s):$(tput sgr0)"
-terraform output -state=${TFDIR}${CUSTOMERPREFIX}.tfstate -module=${MODULE} k8s-master.public_ips | sed 's/,/\n/g'
+terraform output -state=${TFDIR}/${CUSTOMERPREFIX}.tfstate -module=${MODULE} k8s-master.public_ips | sed 's/,/\n/g'
 echo "$(tput setaf 2)Nodes:$(tput sgr0)"
-terraform output -state=${TFDIR}${CUSTOMERPREFIX}.tfstate -module=${MODULE} k8s-node.public_ips | sed 's/,/\n/g'
+terraform output -state=${TFDIR}/${CUSTOMERPREFIX}.tfstate -module=${MODULE} k8s-node.public_ips | sed 's/,/\n/g'
