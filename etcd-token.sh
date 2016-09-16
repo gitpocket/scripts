a01c5b35b80fad04e99aba8bff2fb3bb6fb55913
Dennis Non <dennis.non@kpn.com>
2016-09-16 12:59:55 +0200

    update scripts

--- etcd-token.sh
+++ etcd-token.sh
@@ -6,7 +6,6 @@ fi
 
 CLUSTERSIZE=${1:-"3"}
 
-TOKEN=$(curl -s https://discovery.etcd.io/new?size=${CLUSTERSIZE} | awk -F/ '{print $4}')
-
 echo "$(tput setaf 2)New ETCD Discovery Token:$(tput sgr0)"
+TOKEN=$(curl -s https://discovery.etcd.io/new?size=${CLUSTERSIZE} | awk -F/ '{print $4}')
 echo $TOKEN
