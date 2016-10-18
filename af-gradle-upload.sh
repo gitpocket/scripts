50b094770067b9eff095cb24c458f3d282bbf26f
Dennis Non <dennis.non@kpn.com>
2016-10-18 15:16:18 +0200

    update

--- af-gradle-upload.sh
+++ af-gradle-upload.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-lsof -i :8081 | grep LISTEN &> /dev/null
+
+if [ -z $1 ]
+then
+  echo "$(tput setaf 1)No file provided$(tput sgr0)"
+  exit 1
+fi
+
+lsof -i :8082 | grep LISTEN &> /dev/null
 if [ $? != "0" ]; then
-    echo "No tunnel to Jenkins on port 8081"
+    echo "No tunnel to Jenkins on port 8082"
     exit 1
 fi
 
-JENKINS_ADDRESS="http://localhost:8081/"
+JENKINS_ADDRESS="http://localhost:80822/"
 GRADLEW_PATH="/home/$USER/appfactory/pipeline"
+FILE_PATH=$(readlink -f $1)
 
 if [ -z $IPA_USER ]
 then
@@ -26,4 +34,4 @@ fi
 
 cd $GRADLEW_PATH
 
-$GRADLEW_PATH/gradlew rest -Dpattern=$1 -DbaseUrl=$JENKINS_ADDRESS -Dusername=$USERNAME -Dpassword=$PASSWORD
+$GRADLEW_PATH/gradlew rest -Dpattern=$FILE_PATH -DbaseUrl=$JENKINS_ADDRESS -Dusername=$USERNAME -Dpassword=$PASSWORD
